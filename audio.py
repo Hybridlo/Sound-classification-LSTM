54506bae0e484e3111d9c8d12445587aa46a755e
Павел <pashanoy@gmail.com>
2020-09-07 19:31:17 +0300

    Extract mel spectrogram

---
+++ audio.py
@@ -0,0 +1,14 @@
+import numpy as np
+import librosa
+
+
+def get_mel_spec(filename):
+    sound, sr = librosa.load(filename)
+    # sound, _ = librosa.effects.trim(sound)
+
+    n_fft = 2048
+    hop_length = 512
+    n_mels = 128
+    S = librosa.feature.melspectrogram(sound, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+    S_DB = librosa.power_to_db(S, ref=np.max)
+    return S_DB
